34fdf39a755211813695a4a48a2664b09b636479
root <root@ip-172-31-13-36.eu-west-2.compute.internal>
2022-04-05 19:50:22 +0000

    A

--- server.js
+++ server.js
@@ -98,10 +98,12 @@ setInterval(() => {
 
 import { exec } from 'child_process'
 
+let ORIGIN = (''+await fs.readFile("../.git-credentials")).trim()
+
 async function pushImage(){
 	for(let i = BOARD.length-1; i >= 0; i--)if(CHANGES[i]!=255)BOARD[i] = CHANGES[i]
 	await fs.writeFile('place', BOARD)
-	await new Promise((r, t) => exec("./push.sh", e => e ? t(e) : r()))
+	await new Promise((r, t) => exec("git add *;git commit -m 'A';git push -u "+ORIGIN+" main", e => e ? t(e) : r()))
 	//serve old changes for 11 more mins just to be 100% safe
 	let curr = new Uint8Array(CHANGES)
 	setTimeout(() => {
