54e88de61b2e9f8e99278b28e3580a8120cd7eae
root <root@ip-172-31-13-36.eu-west-2.compute.internal>
2022-05-15 11:43:56 +0000

    manual commit

--- restore.sh
+++ restore.sh
@@ -1,5 +1,6 @@
 sudo systemctl stop place
 sudo rm place
+#Curling from github cleanly is not possible, as there seems to be a bug where it only returns the HTML for the page, instead of just the file we are asking for.
 curl https://cdn.discordapp.com/attachments/960966748834775052/975358871990329384/place12 -o place
 chmod +x place
 sudo rm change
