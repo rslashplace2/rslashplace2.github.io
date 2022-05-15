b99b0a25d66d4f95330f7a972c06018dae9f7d38
root <root@ip-172-31-13-36.eu-west-2.compute.internal>
2022-05-15 12:28:30 +0000

    Hourly backup

--- restore.sh
+++ restore.sh
@@ -1,7 +1,7 @@
 sudo systemctl stop place
 sudo rm place
 #Curling from github cleanly is not possible, as there seems to be a bug where it only returns the HTML for the page, instead of just the file we are asking for.
-curl $1 -o place
+curl https://raw.githubusercontent.com/rslashplace2/rslashplace2.github.io/$1/place -o place
 chmod +x place
 sudo rm change
 sudo systemctl start place
