0fb39e482afd6db435b41ec5e9d7470098a33080
root <root@ip-172-31-13-36.eu-west-2.compute.internal>
2022-05-16 12:33:31 +0000

    Hourly backup

--- server.js
+++ server.js
@@ -250,6 +250,7 @@ function clearPreban() {
 
 function checkPreban(incomingX, incomingY, ip) {
 	if (!(prebanArea.x == 0 && prebanArea.y == 0 && prebanArea.x1 == 0 && prebanArea.y1 == 0)) {
+		console.log("At least it got to the checking stage " + incomingX + " " + incomingY + " " + ip)
 		if ((incomingX > prebanArea.x && incomingX < prebanArea.x1) && (incomingY > prebanArea.y && incomingY < prebanArea.y1)) {
 			if (prebanArea.banPlaceAttempts) {
 				BANS.add(ip)
