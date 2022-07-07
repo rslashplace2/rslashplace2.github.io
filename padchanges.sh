4aa8eff776946b9b703472f77ac44644301063be
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2022-07-07 17:36:04 +0100

    Include use_git and fixes

--- padchanges.sh
+++ padchanges.sh
@@ -2,5 +2,5 @@
 #This script attempts to fix a corrupted board by padding it's changes file.
 sudo push
 sudo systemctl stop place
-node -e "fs=require('fs'); let {WIDTH, HEIGHT, PALETTE_SIZE, COOLDOWN} = JSON.parse(fs.readFile('./config.json')); let CHANGES = fs.readFile('change'); CHANGES=Buffer.concat(CHANGES,Buffer.of(WIDTH*HEIGHT-CHANGES.length))"
+node -e "fs=require('fs'); let {WIDTH, HEIGHT, PALETTE_SIZE, COOLDOWN, USE_GIT} = JSON.parse(fs.readFile('./config.json')); let CHANGES = fs.readFile('change'); CHANGES=Buffer.concat(CHANGES,Buffer.of(WIDTH*HEIGHT-CHANGES.length))"
 sudo systemctl start place
