968233690b98ae61149fc25dda7e9d8710ae95c0
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2022-08-17 17:06:27 +0100

    daemonise fix

--- daemonise.sh
+++ daemonise.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 # This script will alow rplacetk to always run in the background, and auto load on reboot using a systemD unit (only works on linux)
 
-if [ -z "$1" ] then
-    echo "Please input the path to the rslashplace2.github.io/rplace server directory"
+if [ -z "$1" ]
+then
+    echo -e "\x1b[31mPlease input the path to the rplace server directory as an arguument e.g home/pi/rslashplace2.github.io"
     exit 0
 fi
 
 echo -e "
-[Unit]\n
-Description=main place websocket server, see http-place for the static http server.\n
-After=network.target\n
-\n
-[Service]\n
-Type=simple\n
-StandardInput=tty-force\n
-TTYVHangup=yes\n
-TTYPath=/dev/tty20\n
-TTYReset=yes\n
-WorkingDirectory=$1\n
-ExecStart=/usr/bin/node server.js\n
-Restart=always\n
-RestartSec=2\n
-\n
-[Install]\n
-WantedBy=multi-user.target\n
+[Unit]
+Description=main place websocket server, see http-place for the static http server.
+After=network.target
+
+[Service]
+Type=simple
+StandardInput=tty-force
+TTYVHangup=yes
+TTYPath=/dev/tty20
+TTYReset=yes
+WorkingDirectory=$1
+ExecStart=/usr/bin/node server.js
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
 " | sudo tee -a /etc/systemd/system/place.service
+sudo systemctl daemon-reload
 
 echo -e "
-[Unit]\n
-Description=place-http for the static http server, see place for main websocket server.\n
-After=network.target\n
-\n
-[Service]\n
-Type=simple\n
-StandardInput=tty-force\n
-TTYVHangup=yes\n
-TTYPath=/dev/tty21\n
-TTYReset=yes\n
-WorkingDirectory=$1/place_http_server/\n
-ExecStart=/usr/bin/node index.js\n
-Restart=always\n
-RestartSec=2\n
-\n
-[Install]\n
-WantedBy=multi-user.target\n
-" | sudo tee -a /etc/systemd/system/http-place.service
+[Unit]
+Description=place-http for the static http server, see place for main websocket server.
+After=network.target
 
+[Service]
+Type=simple
+StandardInput=tty-force
+TTYVHangup=yes
+TTYPath=/dev/tty21
+TTYReset=yes
+WorkingDirectory=$1/place_http_server/
+ExecStart=/usr/bin/node index.js
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee -a /etc/systemd/system/http-place.service
 sudo systemctl daemon-reload
+
 sudo systemctl enable place.service http-place.service
 sudo systemctl start place.service http-place.service
+
+echo "Task completed."
