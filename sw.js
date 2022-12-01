c109dd7358b7d6bd3fa71505322f9acce9d67508
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2022-12-01 13:36:39 +0000

    Create service worker for PWA install

---
+++ sw.js
@@ -0,0 +1,16 @@
+// Service worker for PWA installation
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+        .open("v1")
+        .then((cache) =>
+            cache.addAll([
+            "/",
+            "/index.html",
+            "/styles.css",
+            "/custom_emojis/fan.png",
+            ])
+        )
+    )
+})
