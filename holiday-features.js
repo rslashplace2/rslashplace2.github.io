eef7c0daecb4c50f53166b95865420c643200416
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2024-01-06 13:26:46 +0000

    Fix bug in import

--- holiday-features.js
+++ holiday-features.js
@@ -1,18 +1,19 @@
-if (new Date().getMonth() === 11) {
-    import CanvasSnow from './canvas-snow.esm.min.js'
-
-    console.log(
-        "%cHappy Holidays!",
-        "font-size: 24px;color: #c00; background-color: #fff; padding: 10px; border: 2px solid #00f; border-radius: 5px; text-shadow: 1px 1px 0 #c00, 2px 2px 0 #00f, 3px 3px 0 #c00, 4px 4px 0 #00f, 5px 5px 0 #c00, 6px 6px 0 #00f, 7px 7px 0 #c00, 8px 8px 0 #00f, 9px 9px 0 #c00, 10px 10px 0 #00f, 11px 11px 0 #c00, 12px 12px 0 #00f, 13px 13px 0 #c00, 14px 14px 0 #00f, 15px 15px 0 #c00, 16px 16px 0 #00f, 17px 17px 0 #c00, 18px 18px 0 #00f, 19px 19px 0 #c00, 20px 20px 0 #00f"
-    )
-    const hat = document.createElement("img")
-    hat.src = "svg/santa-hat.svg"
-    hat.setAttribute("style", "position: absolute;  height: 128px; z-index: 22;left: calc(50% - 160px);top: calc(50% - 110px);transform: rotate(-30deg);")
-    document.getElementById("loadingScreen").appendChild(hat)
-
-    const canvasSnow = new CanvasSnow({
-        context: "#bgWrapper",
-        cell: 30
-    }).init()
-    canvasSnow.start()
-}
+(async function(){
+    if (new Date().getMonth() === 11) {
+        const CanvasSnow = await import('./canvas-snow.esm.min.js')
+        console.log(
+            "%cHappy Holidays!",
+            "font-size: 24px;color: #c00; background-color: #fff; padding: 10px; border: 2px solid #00f; border-radius: 5px; text-shadow: 1px 1px 0 #c00, 2px 2px 0 #00f, 3px 3px 0 #c00, 4px 4px 0 #00f, 5px 5px 0 #c00, 6px 6px 0 #00f, 7px 7px 0 #c00, 8px 8px 0 #00f, 9px 9px 0 #c00, 10px 10px 0 #00f, 11px 11px 0 #c00, 12px 12px 0 #00f, 13px 13px 0 #c00, 14px 14px 0 #00f, 15px 15px 0 #c00, 16px 16px 0 #00f, 17px 17px 0 #c00, 18px 18px 0 #00f, 19px 19px 0 #c00, 20px 20px 0 #00f"
+        )
+        const hat = document.createElement("img")
+        hat.src = "svg/santa-hat.svg"
+        hat.setAttribute("style", "position: absolute;  height: 128px; z-index: 22;left: calc(50% - 160px);top: calc(50% - 110px);transform: rotate(-30deg);")
+        document.getElementById("loadingScreen").appendChild(hat)
+    
+        const canvasSnow = new CanvasSnow({
+            context: "#bgWrapper",
+            cell: 30
+        }).init()
+        canvasSnow.start()
+    }
+})()
