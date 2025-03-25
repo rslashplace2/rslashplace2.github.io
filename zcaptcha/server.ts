cc19b8aeb0c71348b6b14d1caf86600a61c01eda
Zekiah-A <zekiahamoako@outlook.com>
2025-03-25 03:06:18 +0000

    Update bun packages & correct zcaptcha FFI calls to be more sane

--- zcaptcha/server.ts
+++ zcaptcha/server.ts
@@ -1,8 +1,8 @@
 /* eslint-disable jsdoc/require-jsdoc */
 /* eslint-disable no-unused-vars */
-import { dlopen, FFIType, suffix, CString, read, toArrayBuffer, ptr, Pointer } from 'bun:ffi';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { dlopen, FFIType, suffix, CString, read, toArrayBuffer, Pointer, ptr } from "bun:ffi"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const libPath = join(__dirname, "ZCaptcha." + suffix)
@@ -43,23 +43,19 @@ function copy(src:ArrayBuffer)  {
 }
 
 function decodeGenResult(genResult:Pointer) {
-    // HACK: Because bun is a complete 🤡 shitshow of a runtime, if we try read these POINTER
-    // props as ptrs, we get a runtime crash, yet it works perfectly if we read as i64??!?
-    const answerPtr = read.i64(genResult, 0)
-    const dummiesPtr = read.i64(genResult, 8)
-    const answerStr = new CString(Number(answerPtr))
-    const dummiesStr = new CString(Number(dummiesPtr))
-    const imgPtr = read.i64(genResult, 16)
+    const answerStr = new CString(genResult, 0)
+    const dummiesStr = new CString(genResult, 8)
     const imgLen = read.u32(genResult, 24)
-    const imageData = copy(toArrayBuffer(Number(imgPtr), 0, imgLen))
+    const imageData = copy(toArrayBuffer(genResult, 16, imgLen))
 
     return { answer: answerStr.toString(), dummies: dummiesStr.toString(), data: imageData }
 }
 
 export function init() {
     const pathStr = join(__dirname, "Data/NotoColorEmoji-Regular.ttf")
-    const strBuffer = Buffer.from(pathStr, "utf8")
-    const result = initialise(strBuffer)
+    const pathStrBuffer = new Uint8Array(new TextEncoder().encode(pathStr))
+    const cPathStr = new CString(ptr(pathStrBuffer))
+    const result = initialise(cPathStr)
     if (result == -1) {
         throw new Error("Could not initialise zcaptcha native module. Invalid font path?")
     }
